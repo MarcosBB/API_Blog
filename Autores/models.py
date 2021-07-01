9c98c173fe7fd4f55b3bb8f5f3e00ce01789baae
Marcos Beraldo Barros <marcosberaldobarros@hotmail.com>
2021-07-01 16:01:12 -0400

    Criando models

--- Autores/models.py
+++ Autores/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from uuid import uuid4
 
-# Create your models here.
+class Autor(models.Model):
+    id_autor = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    nome = models.CharField('Nome',max_length=255)
+    sobrenome = models.CharField('Sobrenome',max_length=255)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return self.nome
--- Publicacoes/models.py
+++ Publicacoes/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.base import Model
+from uuid import uuid4
 
-# Create your models here.
+class Publicacao(models.Model):
+    id_publicacao = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    titulo = models.CharField('Titulo',max_length=255)
+    descricao = models.TextField('Descrição', max_length=200)
+    autor = models.ForeignKey('Autores.Autor', verbose_name='Autor', on_delete=models.CASCADE)
+    criado = models.DateField('Criação', auto_now_add=True)
+    modificado = models.DateField('Atualização', auto_now=True)
+    
+    class Meta:
+        verbose_name = 'Publicação'
+        verbose_name_plural = 'Publicações'
+
+    def __str__(self):
+        return self.titulo
